3a3df155988c5890da6fa600beb34a24829c5290
isobelojalvo <ojalvo@wisc.edu>
2017-07-19 12:26:18 -0500

    adding in the tau producer cff

---
+++ python/L1PFTauProducer_cff.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+L1PFTauProducer = cms.EDProducer("L1PFTauProducer",
+                                 debug           = cms.untracked.bool(False),
+                                 EoH_cut         = cms.untracked.int32(2),
+                                 HoE_cut         = cms.untracked.int32(8),
+                                 L1Clusters      = cms.InputTag("L1CaloClusterProducer","L1Phase2CaloClusters"),
+                                 L1TrackInputTag = cms.InputTag("TTTracksFromTracklet", "Level1TTTracks"),            
+                                 L1PFObjects     = cms.InputTag("L1PFProducer","L1PFObjects"),
+                                 L1Neutrals      = cms.InputTag("L1PFProducer", "L1PFObjects")
+                                 )
